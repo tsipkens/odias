94afe6deb03da64cef3d55bc0930ecb266b5e5f0
tsipkens <tsipkens@gmail.com>
2021-04-01 01:17:37 -0400

    Add Tikhonov optimization
    + Refactor main.m

--- +invert/tikhonov_op.m
+++ +invert/tikhonov_op.m
@@ -1,8 +1,7 @@
 
 % TIKHONOV_OP  Finds optimal lambda for Tikhonov solver using known distribution, x.
-% Author: Timothy Sipkens, 2019-07-17
 % 
-% Inputs:
+%  INPUTS:
 %   A       Model matrix
 %   b       Data
 %   n_grid  Length of first dimension of solution
@@ -11,13 +10,14 @@
 %   order   Order of regularization     (Optional, default is 1)
 %   n       Number of lambda entries    (Optional, default is 70)
 %
-% Outputs:
+%  OUTPUTS:
 %   x       Regularized estimate
 %   lambda  Semi-optimal regularization parameter
 %           (against exact solution if x_ex is specified or using Bayes factor)
 %   output  Output structure with information for a range of the regularization parameter
 %   Gpo_inv     Inverse of posterior covariance
-%=========================================================================%
+%  
+%  AUTHOR: Timothy Sipkens, 2019-07-17
 
 function [x,lambda,output,Gpo_inv] = tikhonov_op(A,b,span,order,n,~,solver)
 
@@ -32,16 +32,16 @@ if isempty(n); n = 70; end % default number of lambda entries to consider
 lambda = logspace(log10(span(1)),log10(span(2)),n);
 x_length = size(A,2);
 
-Lpr0 = invert1d.tikhonov_lpr(order,x_length); % get Tikhonov matrix
+Lpr0 = invert.tikhonov_lpr(order,x_length); % get Tikhonov matrix
 
-disp('Pre-computing generalized SVD...');
+disp('Pre-computing GSVD ...');
 [~,~,~,S1,S2] = gsvd(full(A),full(Lpr0));
     % pre-compute gsvd for Bayes factor calculation
-disp('Complete.');
+tools.textdone();
 disp(' ');
 
-disp('Optimizing Tikhonov regularization w.r.t lambda...');
-tools.textbar(0);
+disp('Optimizing Tikhonov w.r.t λ:');
+tools.textbar([0, length(lambda)]);
 for ii=length(lambda):-1:1 % reverse loop to pre-allocate
     output(ii).lambda = lambda(ii); % store regularization parameter
     
@@ -54,10 +54,10 @@ for ii=length(lambda):-1:1 % reverse loop to pre-allocate
     
     %-- Compute credence, fit, and Bayes factor --%
     [output(ii).B,output(ii).F,output(ii).C] = ...
-        optimize.bayesf_precomp(...
+        invert.bayesf_precomp(...
         A,b,output(ii).x,Lpr0,lambda(ii),S1,S2,order);
     
-    tools.textbar((length(lambda)-ii+1)/length(lambda));
+    tools.textbar([length(lambda)-ii+1, length(lambda)]);
 end
 
 [~,ind_min] = max([output.B]); % use Bayes factor
--- main.m
+++ main.m
@@ -15,61 +15,92 @@ A = kernel.gen_dma(d_star, d, prop_dma);
 
 
 mu_d = 200;
-s_d = 1.25;
+s_d = 1.4;
 
 % Unimodal.
 % x0 = normpdf(log10(d), log10(mu_d), log10(s_d));
 
 % Bimodal.
 x0 = normpdf(log10(d), log10(mu_d), log10(s_d)) + ...
-    0.5 .* normpdf(log10(d), log10(mu_d / 3), log10(s_d * 1.1));
+    0.5 .* normpdf(log10(d), log10(mu_d / 3), log10(s_d / 1.15));
 
 b0 = A * x0;
 
-[b, Lb] = tools.get_noise(b0, 1e5, 1e-6);
+[b, Lb] = tools.get_noise(b0, 1e3, 1e-6);
 
 
 figure(1);
-semilogx(d_star,b,'.');
+semilogx(d_star, b, '.');
 hold on;
-semilogx(d_star,b0,'Color',[0.5,0.5,0.5]);
+semilogx(d_star, b0, 'Color', [0.5,0.5,0.5]);
 hold off;
 
 
+%%
+
+disp(' ');
+
 %-- Least-squares ---------%
-% x_lsq = invert.lsq(A,b);
+disp('Running least-squares ...');
+x_lsq = invert.lsq(Lb*A, Lb*b);
+tools.textdone();
+disp(' ');
 
 
 %-- 1st order Tikhonov ----%
-disp('Performing Tikhonov ...');
-lambda_tk1 = 2e1;
-[x_tk1,~,~,Gpo_inv_tk1] = invert.tikhonov(Lb*A,Lb*b,lambda_tk1,1);
+disp('Running Tikhonov (1st) ...');
+lambda_tk1 = 3.3e1;
+[x_tk1, ~, ~, Gpo_inv_tk1] = ...
+    invert.tikhonov(Lb*A, Lb*b, lambda_tk1, 1);
 Gpo_tk1 = inv(Gpo_inv_tk1);
+e.tk1 = (x_tk1 - x0)' * Gpo_inv_tk1 * (x_tk1 - x0);
+tools.textdone();
+disp(' ');
+
+
+%-- 2nd order Tikhonov ----%
+disp('Running Tikhonov (2nd) ...');
+lambda_tk2 = 5e2;
+[x_tk2, ~, ~, Gpo_inv_tk2] = ...
+    invert.tikhonov(Lb*A, Lb*b, lambda_tk2, 2);
+Gpo_tk2 = inv(Gpo_inv_tk2);
+e.tk2 = (x_tk2 - x0)' * Gpo_inv_tk2 * (x_tk2 - x0);
 tools.textdone();
 disp(' ');
 
 
 %-- Exponential distance --%
-disp('Performing exponential distance ...');
+disp('Running exponential distance ...');
 lambda_ed = 5e0;
-ld = 1.1.*log10(s_d);
-[x_ed,~,~,Gpo_inv_ed] = invert.exp_dist(Lb*A,Lb*b,lambda_ed,ld,d);
+ld = 1.2 .* log10(s_d);
+[x_ed, ~, ~, Gpo_inv_ed] = ...
+    invert.exp_dist(Lb*A, Lb*b, lambda_ed, ld, d);
 Gpo_ed = inv(Gpo_inv_ed);
+e.ed = (x_ed - x0)' * Gpo_inv_ed * (x_ed - x0);
 tools.textdone();
+disp(' ');
 disp(' ');
 
 
+e
 
+
+
+%%
 figure(2);
-semilogx(d,x0);
+semilogx(d, x0, 'k--');
 hold on;
-semilogx(d,x_tk1);
-semilogx(d,x_tk1+sqrt(diag(Gpo_tk1)),'r--');
-semilogx(d,max(x_tk1-sqrt(diag(Gpo_tk1)),0),'r--');
 
-semilogx(d,x_ed);
-semilogx(d,x_ed+sqrt(diag(Gpo_ed)),'y--');
-semilogx(d,max(x_ed-sqrt(diag(Gpo_ed)),0),'y--');
+x_tk = x_tk2;
+Gpo_tk = Gpo_tk2;
+semilogx(d, x_tk, 'c');
+semilogx(d, x_tk + 2 .* sqrt(diag(Gpo_tk)),'c--');
+semilogx(d, max(x_tk - 2 .* sqrt(diag(Gpo_tk)),0), 'c--');
+
+semilogx(d, x_ed, 'Color', [0.7,0.7,0.7]);
+semilogx(d, x_ed + 2 .* sqrt(diag(Gpo_ed)), '--', 'Color', [0.7,0.7,0.7]);
+semilogx(d, max(x_ed - 2 .* sqrt(diag(Gpo_ed)),0), '--', 'Color', [0.7,0.7,0.7]);
+
 hold off;
 
 
