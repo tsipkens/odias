1025750333386f8504ecfc56c3371d4b6ceae2ed
tsipkens <tsipkens@gmail.com>
2021-03-31 22:25:11 -0400

    Refactoring of main.m

--- main.m
+++ main.m
@@ -1,26 +1,32 @@
 
-% MAIN  Considers simulations of 1D inversion of aerosol distributions.
+% MAIN  Inversion of mobility distributions.
 % AUTHOR: Timothy Sipkens, 2020-04-11
 %=========================================================================%
 
 addpath cmap;
 
-d = logspace(log10(10),log10(1e3),400)';
-d_star = logspace(log10(10),log10(1e3),150)';
+d = logspace(log10(10),log10(1e3),400)';  % reconstruction points
+d_star = logspace(log10(10),log10(1e3),150)';  % mobility setpoints
 
 prop_dma = kernel.prop_dma;
 
 
-A = kernel.gen_dma(d_star,d,prop_dma);
+A = kernel.gen_dma(d_star, d, prop_dma);
 
 
 mu_d = 200;
 s_d = 1.25;
-x0 = normpdf(log10(d),log10(mu_d),log10(s_d));
-x0_alt = d.*log(10).*lognpdf(d,log(mu_d),log(s_d));
-b0 = A*x0;
 
-[b,Lb] = tools.get_noise(b0,1e5,1e-6);
+% Unimodal.
+% x0 = normpdf(log10(d), log10(mu_d), log10(s_d));
+
+% Bimodal.
+x0 = normpdf(log10(d), log10(mu_d), log10(s_d)) + ...
+    0.5 .* normpdf(log10(d), log10(mu_d / 3), log10(s_d * 1.1));
+
+b0 = A * x0;
+
+[b, Lb] = tools.get_noise(b0, 1e5, 1e-6);
 
 
 figure(1);
@@ -35,21 +41,21 @@ hold off;
 
 
 %-- 1st order Tikhonov ----%
-disp('Performing Tikhonov...');
+disp('Performing Tikhonov ...');
 lambda_tk1 = 2e1;
 [x_tk1,~,~,Gpo_inv_tk1] = invert.tikhonov(Lb*A,Lb*b,lambda_tk1,1);
 Gpo_tk1 = inv(Gpo_inv_tk1);
-disp('Complete.');
+tools.textdone();
 disp(' ');
 
 
 %-- Exponential distance --%
-disp('Performing exponential distance...');
+disp('Performing exponential distance ...');
 lambda_ed = 5e0;
 ld = 1.1.*log10(s_d);
 [x_ed,~,~,Gpo_inv_ed] = invert.exp_dist(Lb*A,Lb*b,lambda_ed,ld,d);
 Gpo_ed = inv(Gpo_inv_ed);
-disp('Complete.');
+tools.textdone();
 disp(' ');
 
 
