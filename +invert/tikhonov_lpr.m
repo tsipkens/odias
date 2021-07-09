dc114fbc57425badf67293dedad471616ed31595
tsipkens <tsipkens@gmail.com>
2021-07-09 16:06:24 -0400

    Add bc input to tikhonov_lpr
    Allows for adjusting of boundary conditions implied by Tikhonov matrix.

--- +invert/tikhonov_lpr.m
+++ +invert/tikhonov_lpr.m
@@ -5,6 +5,9 @@
 %  the order specified in ORDER and corresponding to a vector x of length
 %  X_LENGTH, such that L*x = 0.
 %  
+%  L = tikhonov_lpr(ORDER, X_LENGTH, BC) add an input for the type of
+%  boundary conidition to be applied at the edges.
+%  
 %  ------------------------------------------------------------------------
 %
 %  ORDER options:
@@ -12,11 +15,21 @@
 %   1: 1st order Tikhonov minimizes the first derivative.
 %   2: 2nd order Tikhonov minimized the second derivative.
 %  
+%  BC (boundary condition) options:
+%   []: No explicit boundary conditions (uses that implied by order).
+%    0: Zero BCs.
+%    1: No slope BCs
+%    2: No curvature BCs.
+%  
 %  ------------------------------------------------------------------------
 % 
 %  AUTHOR:   Timothy Sipkens, 2020-04-11
 
-function L = tikhonov_lpr(order, x_length)
+function L = tikhonov_lpr(order, x_length, bc)
+
+% Parse type input.
+if ~exist('bc', 'var'); bc = []; end
+if isempty(bc); bc = order; end  % neglects boundary conditions
 
 % Choose between order of Tikhonov operator to generate.
 switch order
@@ -26,13 +39,25 @@ switch order
     case 1 % 1st order Tikhonov
         L = -speye(x_length);
         L = spdiags(ones(x_length, 1), 1, L);
-        L(end,:) = [];
+        
+        if bc~=0
+            L(end,:) = [];
+        end
         
     case 2 % 2nd order Tikhonov
         L = -speye(x_length);
         L = spdiags(0.5 .* ones(x_length,2), [-1,1], L);
-        L(1,:) = [];
-        L(end,:) = [];
+        
+        if bc == 0  % zero
+            L(1, 2) = 0;
+            L(x_length, x_length - 1) = 0;
+       elseif bc == 1  % no slope
+            L(1, 2) = 1;
+            L(x_length, x_length - 1) = 1;
+        else
+            L(1,:) = [];
+            L(end,:) = [];
+        end
         
     otherwise
         disp('The specified order of Tikhonov is not available.');
