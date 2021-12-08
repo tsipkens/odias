cecc32a5074f4d61fa89f01cf1ba8782f92d270d
tsipkens <tsipkens@gmail.com>
2021-12-08 11:16:31 -0500

    IAC commits Dec. 7

--- +iac/main_iac_perturb.m
+++ +iac/main_iac_perturb.m
@@ -9,7 +9,7 @@ addpath cmap tfer_pma;
 
 
 % Defaults.
-Rm = 3;
+Rm0 = 3;
 nit = 4e13;
 eps0 = 13.5;
 
@@ -20,13 +20,13 @@ Q = 0.9183;
 
 
 % Set charging model parameters. 
-opt.nit = nit;
-opt.eps = eps0;
+opt0.nit = nit;
+opt0.eps = eps0;
 
 
 % Set transfer function evaluation grid.
-nx = 900;  nb = 450;
-m = logspace(-4.5, 3, nx)';  % reconstruction points
+nx = 900;  nb = 400;
+m = logspace(-5, 3, nx)';  % reconstruction points
 m_star = logspace(-4, 1, nb)';  % mass-to-charge setpoints
 
 
@@ -39,20 +39,31 @@ prop0 = working.prop_update_massmob(prop0, ...
 
 
 % Run the IAC algorithm. 
-[m_star_iac] = working.iac(m_star, prop0, [], 'Fuchs', opt);
+m_star_iac0 = working.iac(m_star, prop0, [], 'Fuchs', opt0);
 
 %-{
 % Full charging model. VERY SLOW for all of the points!
 sel = 1:25:length(m_star);
-m_star_fac = working.fac(m_star(sel), prop0, [], [], opt);
+m_star_fac = working.fac(m_star(sel), prop0, [], [], opt0);
 %}
 
 
 %%
 
-cfg = tools.load_config('+iac/config/v1.sig.json');
+% cfg = tools.load_config('+iac/config/v1.sig.json');
 % cfg = tools.load_config('+iac/config/v1.Rm.json');
+% cfg = tools.load_config('+iac/config/v1.mm.rho.json');
+% cfg = tools.load_config('+iac/config/v1.mm.Dm.json');
+% cfg = tools.load_config('+iac/config/v1.nit.json');
+cfg = tools.load_config('+iac/config/v1.nit.b.json');
 
+% If not a field, indicate the the full transfer function
+% and IAC methods are not being perturbed (only the full
+% trasnfer function approach). 
+if ~isfield(cfg, 'both')
+    cfg.both = 0;
+end
+cfg  % show configuration
 
 type = cfg.tfer_type;
 prop = working.prop_update_massmob(prop0, ...
@@ -63,21 +74,46 @@ scan_vec = cfg.scan;
 
 % span_vec = [0.5, 0.75, 1, 1.25, 1.5] .* rho100_0;
 
+m_star_iac = [];
 A_bar = {};
 if ~strcmp(cfg.perturb, 'distr')
     for kk=1:length(scan_vec)
         % For changing resolution.
         if strcmp(cfg.perturb, 'Rm')
             Rm = scan_vec(kk);
+        else
+            Rm = Rm0;
         end
-
+        
+        if strcmp(cfg.perturb, 'nit')
+            opt = opt0;
+            opt.nit = scan_vec(kk) .* 1e13;
+        else
+            opt = opt0;
+        end
+        
         % For changing density.
-        if strcmp(cfg.perturb, 'rho')
-            rho100 = scan_vec(kk);
-            prop = working.prop_update_massmob(prop0, ...
-                'Dm', Dm0, 'rho100', rho100);  % universal relation
+        if contains(cfg.perturb, 'mm')
+            if strcmp(cfg.perturb, 'mm.rho')
+                rho100 = scan_vec(kk) .* rho100_0;
+                prop = working.prop_update_massmob(prop0, ...
+                    'Dm', Dm0, 'rho100', rho100);  % universal relation
+            elseif strcmp(cfg.perturb, 'mm.Dm')
+                Dm = scan_vec(kk) .* Dm0;
+                prop = working.prop_update_massmob(prop0, ...
+                    'Dm', Dm, 'rho100', rho100_0);  % universal relation
+            end
         end
-
+    
+        % If changing both the IAC and full transfer 
+        % function outputs. 
+        if cfg.both == 0
+            m_star_iac(kk,:) = m_star_iac0;
+        else
+            m_star_iac(kk,:) = working.iac(m_star, prop, [], 'Fuchs', opt);
+        end
+        
+        
         d_bar = (m .* 1e-18 ./ prop.m0) .^ (1 / prop.Dm);  % get mobility diameters
         sp = get_setpoint(prop, 'm_star', m_star .* 1e-18, 'Rm', Rm);
 
@@ -85,9 +121,9 @@ if ~strcmp(cfg.perturb, 'distr')
         % A_bar{kk} = kernel.gen_pma(sp, m, d_bar, 0', prop, type, [], opt);
     end
 else
-    d_bar = (m .* 1e-18 ./ prop.m0) .^ (1 / prop.Dm);  % get mobility diameters
-    sp = get_setpoint(prop, 'm_star', m_star .* 1e-18, 'Rm', Rm);
-    A_bar{1} = kernel.gen_pma(sp, m, d_bar, (1:300)', prop, type, 'Fuchs', opt);
+    d_bar = (m .* 1e-18 ./ prop0.m0) .^ (1 / prop0.Dm);  % get mobility diameters
+    sp = get_setpoint(prop0, 'm_star', m_star .* 1e-18, 'Rm', Rm);
+    A_bar{1} = kernel.gen_pma(sp, m, d_bar, (1:300)', prop0, type, 'Fuchs', opt0);
 end
 
 %
@@ -133,12 +169,22 @@ for kk=1:length(scan_vec)
     
 end
 
+% Check if transfer function not fully resolved
+% and NaN if truncated.
+x_flag2 = logical([]);
+for kk=1:length(scan_vec)
+    if length(A_bar) == 1; kk2 = 1;
+    else; kk2 = kk; end
+    x_flag2(kk,:) = (A_bar{kk2}(:,1) > (0.001 .* max(A_bar{kk2},[],2)));
+    m_bar_ftf(kk,x_flag2(kk,:)) = NaN;
+end
+
 
 
 figure(80);
 loglog(m_star, m_bar_ftf);
 hold on;
-loglog(m_star, m_star_iac, 'k', 'LineWidth', 2);
+loglog(m_star, m_star_iac, 'k', 'LineWidth', 1);
 loglog(m_star(sel), m_star_fac, 'ko', 'MarkerSize', 5);
 hold off;
 
@@ -146,10 +192,10 @@ hold off;
 figure(81);
 cm = ocean;  cm = cm(1:(end-40), :);
 cmap_sweep(size(m_bar_ftf,1), cm);
-semilogx(m_star, (m_star_iac' ./ m_bar_ftf) - 1);
-hold on; semilogx(m_star, (m_star_iac' ./ m_bar_ftf(1,:)) - 1, 'r--'); hold off;
+semilogx(m_star, (m_star_iac ./ m_bar_ftf) - 1);
+hold on; semilogx(m_star, (m_star_iac(1,:) ./ m_bar_ftf(1,:)) - 1, 'r--'); hold off;
 
-mr = m_star_iac ./ m_star;
+mr = m_star_iac(1,:)' ./ m_star;
 m1 = find(mr > 1.000001, 1) - 1;
 m2 = find(mr > 2, 1);
 m3 = find(mr > 3, 1);
@@ -172,10 +218,10 @@ semilogx(m_star(sel), (m_star_fac' ./ m_bar_ftf(1, sel)) - 1, 'ko', 'MarkerSize'
 hold off;
 
 ylim([-0.3, 0.3]);
-if strcmp(cfg.perturb, 'distr')
-    ylim([-0.5, 1]);
+if or(strcmp(cfg.perturb, 'distr'), strcmp(cfg.perturb, 'mm.Dm'))
+    ylim([-0.5, 0.5]);
 end
-xlim([10^-4, m_star(end-25)]);
+xlim([10^-4, 1]);
 
 
 figure(35);
@@ -194,3 +240,5 @@ hold off;
 
 
 figure(81);  % show Fig. 81 first
+
+
