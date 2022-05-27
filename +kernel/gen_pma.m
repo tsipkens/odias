d67062614c87cec1f96d17f4f0ce7401ea3764ea
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-05-27 13:27:55 -0400

    Update gen_pma.m
    Relative to the submodule update.

--- +kernel/gen_pma.m
+++ +kernel/gen_pma.m
@@ -16,13 +16,14 @@
 %   Lambda      Transfer function
 %   prop        CPMA device settings
 %   f_z         Charge fractions
+%   Lambda_z    Transfer function with individual charge contributions
 % 
 %  ------------------------------------------------------------------------
 % 
 %  AUTHOR: Timothy Sipkens, 2021-10-06
 
 
-function [Lambda, prop, f_z] = gen_pma(sp, m, d, z, prop, opt, varargin)
+function [Lambda, prop, f_z, Lambda_z] = gen_pma(sp, m, d, z, prop, opt, varargin)
 
 addpath tfer_pma; % add mat-tfer-pma package to MATLAB path
 
@@ -53,11 +54,24 @@ fun = str2func(['tfer_',opt]); % call relevant function from submodule
 % For first charge state.
 Lambda = f_z(1,:) .* ...
     fun(sp, m' .* 1e-18, d' .* 1e-9, z(1), prop); % CPMA transfer function
+if nargout > 3
+    Lambda_z = zeros([size(Lambda), length(z)]);
+    Lambda_z(:, :, 1) = Lambda;
+end
 
 % Add additional charge states.
 for ii=2:length(z)
-    Lambda = Lambda + f_z(ii,:) .* ...
+    Lambda_ii = f_z(ii,:) .* ...
         fun(sp, m' .* 1e-18, d' .* 1e-9, z(ii), prop);
+    Lambda = Lambda + Lambda_ii;
+
+    if nargout > 3
+        Lambda_z(:, :, ii) = Lambda_ii;
+    end
+end
+if nargout > 3
+    Lambda_z = permute(Lambda_z, [1, 3, 2]);
 end
 
 end
+
