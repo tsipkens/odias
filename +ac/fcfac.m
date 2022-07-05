00cb6f5f98c453cd1512eb92b3684ad37c766d37
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-07-05 15:10:48 -0400

    Create fcfac.m

---
+++ +ac/fcfac.m
@@ -0,0 +1,32 @@
+
+% FCFAC  Full charge fraction-average charge algorithm.
+%  Not a true average charge function as this method only ignores the
+%  particle size distribution and transfer function. 
+%  
+%  AUTHOR: Timothy Sipkens, 2022-07-05
+
+function [xbar, qbar] = fcfac(x_star, fq, x, q)
+
+% Use second dimension of A for charges. 
+if ~exist('q', 'var'); q = []; end
+if isempty(q); q = 0:(size(fq, 2) - 1); end
+
+
+disp('Running FCFAC...');  % add header to console
+
+% Compute charge fractions at x*q. 
+fq_star = interp2(x, q, fq, x_star .* q, ones(size(x_star)) .* q);
+
+% Get average charge. 
+% nansum's ignore fq where extrapolation would be required above.  
+qbar = nansum(fq_star .* q ./ ...
+    nansum(fq_star, 2), 2);
+
+qbar(nansum(fq_star, 2) < 1) = NaN;  % interpolation failed at these points
+
+xbar = qbar .* x_star;
+
+tools.textdone();  % mark as complete
+disp(' ');
+
+end
