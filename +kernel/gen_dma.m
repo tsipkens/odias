7203224ca75a85b67ce85c43020840132800fa56
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-07-07 11:03:33 -0400

    Add outputs and fix progress bar
    Added f_z, Omega_z, etc.

--- +kernel/gen_dma.m
+++ +kernel/gen_dma.m
@@ -18,7 +18,7 @@
 %  
 %  AUTHOR:	Timothy Sipkens, 2020-03-09
 
-function [Omega] = gen_dma(d_star, d, z, argin_dma, varargin)
+function [Omega, f_z, Omega_z, qbar] = gen_dma(d_star, d, z, argin_dma, argin_z)
 
 
 %-- Parse inputs -----------------%
@@ -28,35 +28,47 @@ if isempty(z); z = (1:4)'; end
 if ~exist('argin_dma', 'var') argin_dma = {}; end
 if ~iscell(argin_dma); argin_dma = {argin_dma}; end
 
-% the rest of the arguments are passed to kernel.tfer_charge(...)
+if ~exist('argin_z', 'var') argin_z = {}; end
+if ~iscell(argin_z); argin_dma = {argin_z}; end
 %---------------------------------%
 
 
 n_b = length(d_star);
 n_i = length(d);
+n_z = length(z);
 
 %== Evaluate particle charging fractions =================================%
-f_z = sparse(kernel.tfer_charge(d .* 1e-9, z, [], varargin{:})); % get fraction charged for d
-n_z = length(z);
+[f_z, qbar] = kernel.tfer_charge(d .* 1e-9, z, [], argin_z{:}); % get fraction charged for d
 
 
 %== Evaluate DMA transfer function =======================================%
 disp('Computing DMA kernel:');
-Omega = sparse(n_b,n_i);
-tools.textbar([0, n_b]);
-for kk=1:n_z  % loop through charge states
-    Omega_z = kernel.tfer_dma( ...
-        d_star' .* 1e-9, ...
-        d .* 1e-9, ...
-        z(kk), argin_dma{:});
+Omega = sparse(n_b, n_i);
+
+if nargout > 2
+    Omega_z = zeros([size(Omega), length(z)]);
+end
+
+tools.textbar([0, n_z]);
+for ii=1:n_z  % loop through charge states
+    Omega_ii = f_z(ii,:) .* kernel.tfer_dma( ...
+        d_star' .* 1e-9, d .* 1e-9, ...
+        z(ii), argin_dma{:});
     
     % Remove numerical noise in kernel.
-    Omega_z(Omega_z<(1e-7.*max(max(Omega_z)))) = 0;
+    Omega_ii(Omega_ii<(1e-7.*max(max(Omega_ii)))) = 0;
     
     % Compile with other charge states.
-    Omega = Omega + f_z(kk,:) .* sparse(Omega_z);
+    Omega = Omega + Omega_ii;
     
-    tools.textbar([kk, n_z]);
+    if nargout > 2
+        Omega_z(:, :, ii) = Omega_ii;
+    end
+    
+    tools.textbar([ii, n_z]);
+end
+if nargout > 2
+    Omega_z = permute(Omega_z, [1, 3, 2]);
 end
 
 disp(' ');
