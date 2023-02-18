8fd820ac31bed57cdd5f069061cd589ad49e8454
Timothy Sipkens <tsipkens@uwaterloo.ca>
2023-02-17 23:59:28 -0500

    Refactor main_ac_pma*

--- +ac/main_ac_pma_perturb.m
+++ +ac/main_ac_pma_perturb.m
@@ -13,7 +13,7 @@ addpath cmap tfer_pma autils;
 
 % Defaults.
 Rm0 = 3;
-nit = 4e13;
+nit = 4e12;
 eps0 = 13.5;
 
 Dm0 = 2.48;
@@ -32,11 +32,11 @@ opt0.n = 2.5;
 
 
 % Set transfer function evaluation grid.
-nx = 1e3;  nb = 700;
-m = logspace(-4.5, 2, nx)';  % reconstruction points
+nx = 2e3;  nb = 700;
+m = logspace(-5, 2, nx)';  % reconstruction points
 m_star = logspace(-4, 0, nb)';  % mass-to-charge setpoints
 
-zvec = 1:300;
+zvec = 0:300;
 
 % Get properties and then update.
 prop0 = kernel.prop_pma;
@@ -56,9 +56,14 @@ sp = get_setpoint(prop0, 'm_star', m_star .* 1e-18, 'Rm', Rm0);
 
 
 %%
+% Kernel ignoring neutrals. 
+Kq0_nn = Kq0(:,2:end,:);  % no neutrals
+K0_nn = squeeze(sum(Kq0_nn, 2));
+zvec_nn = zvec(2:end);
+
 % "True" average transmitted particle size.
-m_bar_t0 = ac.true([], K0, m);
-m_bar_g0 = exp(ac.true([], K0, log(m)));
+m_bar_t0 = ac.true([], K0_nn, m);
+m_bar_g0 = exp(ac.true([], K0_nn, log(m)));
 
 % Run the IAC algorithm with default settings. 
 [m_bar_iac0, q_bar_iac0] = ...
@@ -66,7 +71,7 @@ m_bar_g0 = exp(ac.true([], K0, log(m)));
 
 % Run the FK algorithm with default settings. 
 [m_bar_fkac0, q_bar_fkac0] = ...
-    ac.fk(m_star, Kq0, zvec);
+    ac.fk(m_star, Kq0_nn, m, zvec_nn);
 [m_bar_fcfac0, q_bar_fcfac0] = ...
     ac.fcf(m_star, fq0, m, zvec);
 
@@ -80,7 +85,7 @@ m_bar_g0 = exp(ac.true([], K0, log(m)));
 % Plot errors at default.
 figure(2); clf;
 
-subplot(5, 1, 1:3);
+subplot(5, 1, 1:2);
 plot(m_star, m_bar_iac0);
 hold on;
 plot(m_star, m_bar_fkac0);
@@ -93,7 +98,7 @@ hold off;
 set(gca, 'XScale', 'log', 'YScale', 'log');
 ylim([1e-4, 1e2]);
 
-subplot(5, 1, 4:5);
+subplot(5, 1, 3:5);
 plot(m_star, (m_bar_iac0 - m_bar_t0) ./ m_bar_t0);
 hold on;
 plot(m_star, (m_bar_fkac0 - m_bar_t0) ./ m_bar_t0);
@@ -104,8 +109,23 @@ plot(m_star, (m_bar_g0 - m_bar_t0) ./ m_bar_t0, 'k');
 plot(m_star, (m_star - m_bar_t0) ./ m_bar_t0, 'k');
 yline(0, 'k--');
 hold off;
-ylim([-0.2, 0.1]);  % reset y-axis
+ylim([-0.15, 0.15]);  % reset y-axis
 set(gca, 'XScale', 'log');
+
+
+
+% Transfer function plot.
+figure(3);
+h = pcolor(m_star, m, K0');
+set(h, 'EdgeColor', 'none');
+set(gca, 'XScale', 'log', 'YScale', 'log');
+
+hold on;
+plot(m_star, m_star, 'r-');
+hold off;
+
+cm = ocean;
+colormap(flipud(cm(50:end-2,:)));
 
 
 
@@ -113,9 +133,9 @@ set(gca, 'XScale', 'log');
 %== START: Perturbation analysis =========================================%
 
 % Range of configuration file specifying perturbation scenarios. 
-cfg = tools.load_config('+ac/config/v2.sig1.json');
-% cfg = tools.load_config('+ac/config/v1.mu.json');
-% cfg = tools.load_config('+ac/config/v1.Rm.json');
+% cfg = tools.load_config('+ac/config/v3.sig1.json');  % fast
+% cfg = tools.load_config('+ac/config/v1.mu.json');  % fast
+cfg = tools.load_config('+ac/config/v1.Rm.json');
 % cfg = tools.load_config('+ac/config/v1.mm.rho.json');
 % cfg = tools.load_config('+ac/config/v1.mm.Dm.json');
 % cfg = tools.load_config('+ac/config/v1.nit.b.json');
@@ -198,9 +218,13 @@ for kk=1:sz
         d = (m .* 1e-18 ./ prop.m0) .^ (1 / prop.Dm);  % use mass-mobility relation
         [K, ~, fq, Kq, qbar] = kernel.gen_pma(sp, m, d, zvec, prop, [], 'Fuchs', opt);  % get kernel
         [nu, qp] = ac.get_power_law(qbar, d);
+
+        Kq_nn = Kq(:,2:end,:);  % no neutrals
+        K_nn = squeeze(sum(Kq_nn, 2));
+        zvec_nn = zvec(2:end);
         
         % "True" average transmitted particle size.
-        m_bar_t(kk,:) = ac.true([], K, m);
+        m_bar_t(kk,:) = ac.true([], K_nn, m);
         
         if cfg.both == 1
             % Run the FK algorithm. 
@@ -251,9 +275,9 @@ for kk=1:sz
         end
         pfl = abs((log(m) - log(muk)) ./ log(sigk)) > 3;
 
-        m_bar_t(kk,:) = ac.true([], K0, m, p);
+        m_bar_t(kk,:) = ac.true([], K0_nn, m, p);
         
-        t0 = sum(p' .* K0, 2);  % sum(x_bar .* A_bar{1}, 2);
+        t0 = sum(p' .* K0_nn, 2);  % sum(x_bar .* A_bar{1}, 2);
         x_flag = t0 < (0.05 .* max(t0));
         m_bar_t(kk, x_flag) = NaN;
         
@@ -276,12 +300,8 @@ hold off;
 set(gca, 'XScale', 'log', 'YScale', 'log');
 
 figure(10);
-plot(m_star, (m_bar_plac - m_bar_t) ./ m_bar_t, 'k');
-hold on;
-% plot(m_star, (m_star' - m_bar_t(1,:)) ./ m_bar_t(1,:), 'k');
 plot(m_star, (m_bar_intac - m_bar_t) ./ m_bar_t);
-% plot(m_star, (m_bar_fkac - m_bar_t) ./ m_bar_t, '--');
-ylim([-0.2, 0.1]);
-hold off;
+ylim([-0.25, 0.25]);
+xlim([1e-3, 1e0]);
 set(gca, 'XScale', 'log');
 
